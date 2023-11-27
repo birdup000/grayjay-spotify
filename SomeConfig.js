ea352b22c4085469cac400c35d91940593f3ceba
Birdup <34012548+birdup000@users.noreply.github.com>
2023-11-27 15:11:33 -0600

    Create SomeConfig.js

---
+++ SomeConfig.js
@@ -0,0 +1,23 @@
+{
+	"name": "Some name",
+	"description": "A description for your plugin",
+	"author": "Your author name",
+	"authorUrl": "https://yoursite.com",
+	
+	"sourceUrl": "https://yoursite.com/SomeConfig.json",
+	"repositoryUrl": "https://github.com/someuser/someproject",
+	"scriptUrl": "./SomeScript.js",
+	"version": 1,
+	
+	"iconUrl": "./someimage.png",
+	"id": "309b2e83-7ede-4af8-8ee9-822bc4647a24",
+	
+	"scriptSignature": "<ommitted>",
+	"scriptPublicKey": "<ommitted>",
+	"packages": ["Http"],
+	
+	"allowEval": false,
+	"allowUrls": [
+		"everywhere"
+	]
+}
